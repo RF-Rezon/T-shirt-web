dae2d66ca56f16fd1bd538a9e833e87e4284d335
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-04-07 17:43:08 +0600

    satisfied_two

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Cart = ({ cart, handelRemoveFromCart }) => {
   return (
-    <div className="text-white text-xl font-medium p-5 text-left">
+    <div className="text-white text-xl font-medium p-5 text-left lg:ml-6 mt-2" >
       <div className="text-left">
         <span>Order Summary: </span>
-        <p>{cart.length}</p>
+        <p className="my-3 text-xl font-bold">{cart.length}</p>
       </div>
       <div className="mt-10 lg:mt-0">
         {cart.map((x) => (
-          <p className="border border-orange-600 rounded-lg my-5 p-3 w-1/2 text-center" key={x._id}>
+          <p className="border border-orange-600 rounded-lg mt-10 my-7 p-2 lg:w-1/2 w-full text-center" key={x._id}>
             {x.name}{" "}
             <button
               onClick={() => handelRemoveFromCart(x._id)}
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='space-x-5'>
-           <Link className='px-3 py-2 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black' to="/">Home</Link>
-           <Link className='px-3 py-2 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black'  to="/review">Order Review</Link>
-           <Link className='px-3 py-2 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black'  to="/about">About</Link>
-           <Link className='px-3 py-2 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black'  to="/contact">Contact</Link>
+        <nav className='space-x-5 p-4 text-center pt-10 my-6'>
+           <Link className='px-4 py-3 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black' to="/">Home</Link>
+           <Link className='px-4 py-3 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black'  to="/review">Order Review</Link>
+           <Link className='px-4 py-3 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black'  to="/about">About</Link>
+           <Link className='px-4 py-3 hover:bg-orange-500 hover:border-white hover:border-2 rounded hover:text-black'  to="/contact">Contact</Link>
         </nav>
     );
 };
